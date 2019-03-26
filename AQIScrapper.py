21d4d108a8bfd9564af64862e2682bcf8cb733dc
Matt Erickson <matterickson9819@gmail.com>
2019-03-26 11:45:24 -0500

    Script now renames and moves csv files to their correct directories

--- AQIScrapper.py
+++ AQIScrapper.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-import sys, os, time
+import os
+import time
+import shutil
+
 browser = webdriver.Firefox()
 
 URL = "https://www.epa.gov/outdoor-air-quality-data/air-quality-statistics-report"
 path = "./Data/"
-
+downloads = '/home/matt/Downloads/'
 #Array to store the desired dates
 dates = ["1980","1985","1990","1995","2000","2005","2010","2015","2019"]
 
@@ -23,10 +26,10 @@ states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado",
 browser.get(URL)
 time.sleep(4)
 year = Select(browser.find_element_by_id('year'))
-year.select_by_visible_text('1980')
+year.select_by_visible_text(dates[0])
 
 state = Select(browser.find_element_by_id('state'))
-state.select_by_visible_text('Alabama')
+state.select_by_visible_text(states[0])
 
 browser.find_element_by_css_selector('#sumlevel > div:nth-child(2) > label').click()
 
@@ -35,3 +38,11 @@ browser.find_element_by_css_selector('#launch > input[type="button"]').click()
 time.sleep(10)
 
 browser.find_element_by_css_selector('#results > p:nth-child(2) > a:nth-child(4)').click()
+
+if not os.path.exists(path + states[0] + '/'):
+    os.mkdir(path + states[0] + '/')
+
+destPath = path + states[0] + '/' + states[0] + dates[0] + ".csv"
+srcPath = downloads + '/conreport' + dates[0] + '.csv'
+#Moves the file
+shutil.move(srcPath,destPath)
