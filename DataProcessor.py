e50a887d09a0eada8eaa0c9e5246dd53e9ce2193
Matt Erickson <matterickson9819@gmail.com>
2019-04-02 20:09:43 -0500

    Changed data processing to only get MN data

--- DataProcessor.py
+++ DataProcessor.py
@@ -3,8 +3,9 @@ import pandas as pd
 
 path = "./Data/"
 
-years = ["1980","1982","1984","1986","1988","1990","1992","1994","1996","1998","2000"]
-#
+#years = ["1980","1982","1984","1986","1988","1990","1992","1994","1996","1998","2000"]
+years = ["1980","1981","1982","1983","1984","1985","1986","1987","1988","1989","1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000"]
+
 # count = 0
 #
 # colnames = 'N02 Annual Mean'
@@ -19,30 +20,57 @@ import csv
 
 # open the file in universal line ending mode
 
+# for i in range(1):
+#     for(dirname,dirpath,filenames) in os.walk(path + years[i] + "/"):
+#         count = 0
+#         for files in filenames:
+#             try:
+#                 with open(path + years[0] + "/" + files, 'rU') as infile:
+#                   # read the file as a dictionary for each row ({header : value})
+#                   reader = csv.DictReader(infile)
+#                   data = {}
+#                   for row in reader:
+#                     for header, value in row.items():
+#                       try:
+#                         data[header].append(value)
+#                       except KeyError:
+#                         data[header] = [value]
+#
+#                 mean = 0
+#
+#                 for j in data['NO2 Mean 1-hr']:
+#                     try:
+#                         mean += float(j)
+#                         count += 1
+#                     except:
+#                         print("Mean Not Counted for: ", files)
+#
+#                 print("Mean: ",mean)
+#             except:
+#                 continue
+
 for i in range(len(years)):
-    for(dirname,dirpath,filenames) in os.walk(path + years[i] + "/"):
-        count = 0
+    MNname = "Minnesota" + years[i] + ".csv"
+    for (dirname,dirpath,filenames) in os.walk(path + years[i] + "/"):
         for files in filenames:
-            try:
-                with open(path + years[0] + "/" + files, 'rU') as infile:
-                  # read the file as a dictionary for each row ({header : value})
-                  reader = csv.DictReader(infile)
-                  data = {}
-                  for row in reader:
-                    for header, value in row.items():
-                      try:
-                        data[header].append(value)
-                      except KeyError:
-                        data[header] = [value]
-
+            if files == MNname:
+                with open(path + years[i] + "/" + MNname, 'rU') as infile:
+                    reader = csv.DictReader(infile)
+                    data = {}
+                    for row in reader:
+                        for header, value in row.items():
+                            try:
+                                data[header].append(value)
+                            except:
+                                data[header] = [value]
                 mean = 0
-
-                for j in data['NO2 Mean 1-hr']:
+                count = 0
+                for j in data['SO2 Mean 1-hr']:
                     try:
                         mean += float(j)
+                        count += 1
                     except:
                         continue
+                mean = mean/count
+                print(files, " mean: ", mean)
 
-                print(mean)
-            except:
-                continue
