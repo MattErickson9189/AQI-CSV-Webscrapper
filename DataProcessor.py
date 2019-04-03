5fc24902182d4179de9c46d5b22ea1bbb391e8db
Matt Erickson <matterickson9819@gmail.com>
2019-04-03 11:14:49 -0500

    Now collects data for all the midwest states

--- DataProcessor.py
+++ DataProcessor.py
@@ -8,27 +8,28 @@ years = ["1980","1981","1982","1983","1984","1985","1986","1987","1988","1989","
 desiredStates = ["North Dakota", "South Dakota", "Minnesota", "Wisconsin", "Iowa"]
 
 for i in range(len(years)):
-    MNname = "Minnesota" + years[i] + ".csv"
-    for (dirname,dirpath,filenames) in os.walk(path + years[i] + "/"):
-        for files in filenames:
-            if files == MNname:
-                with open(path + years[i] + "/" + MNname, 'rU') as infile:
-                    reader = csv.DictReader(infile)
-                    data = {}
-                    for row in reader:
-                        for header, value in row.items():
-                            try:
-                                data[header].append(value)
-                            except:
-                                data[header] = [value]
-                mean = 0
-                count = 0
-                for j in data['SO2 Mean 1-hr']:
-                    try:
-                        mean += float(j)
-                        count += 1
-                    except:
-                        continue
-                mean = mean/count
-                print(files, " mean: ", mean)
+    for j in range(len(desiredStates)):
+        name = desiredStates[j] + years[i] + ".csv"
+        for (dirname,dirpath,filenames) in os.walk(path + years[i] + "/"):
+            for files in filenames:
+                if files == name:
+                    with open(path + years[i] + "/" + name, 'rU') as infile:
+                        reader = csv.DictReader(infile)
+                        data = {}
+                        for row in reader:
+                            for header, value in row.items():
+                                try:
+                                    data[header].append(value)
+                                except:
+                                    data[header] = [value]
+                    mean = 0
+                    count = 1
+                    for j in data['SO2 Mean 1-hr']:
+                        try:
+                            mean += float(j)
+                            count += 1
+                        except:
+                            continue
+                    mean = mean/count
+                    print(files, " mean: ", "{0:.2f}".format(mean))
 
